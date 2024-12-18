00f1464f5f7fb28911c937592eeea7aeb848da20
gyeoul <gyeoul@gyeoul.dev>
2024-12-19 00:04:39 +0900

    feat: Update default paths and add DataBulkConvertor to solution
    Updated default file paths in `ResolverTester` and `ExcelGenerator` for consistency across projects. Commented out unused logging code in `ResolverTester`. Added the `DataBulkConvertor` project to the solution for future functionality.

--- ExcelGenerator/Program.cs
+++ ExcelGenerator/Program.cs
@@ -2,7 +2,7 @@ namespace ExcelGenerator;
 
 internal static class Program {
     private static void Main(string[] args) {
-        var gamePath = args.Length >= 1 ? args[0] : @"C:\Steam\steamapps\common\FINAL FANTASY XIV Online\";
+        var gamePath = args.Length >= 1 ? args[0] : @"C:\FF14\";
         var outputPath = args.Length >= 2 ? args[1] : @"..\..\..\FFXIVClientStructs\FFXIV\Component\Excel\Sheets\";
         var schemaPath = args.Length >= 3 ? args[2] : null;
 
