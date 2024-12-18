65d548ff0d7f12518f9879effa772135a5606820
gyeoul <gyeoul@gyeoul.dev>
2024-12-18 17:10:55 +0900

    feat: Update signature for kr

--- FFXIVClientStructs/FFXIV/Client/UI/RaptureAtkModule.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/RaptureAtkModule.cs
@@ -28,8 +28,8 @@ public unsafe partial struct RaptureAtkModule {
 
     [FieldOffset(0x87F7)] public AgentUpdateFlags AgentUpdateFlag; // reset happens in RaptureAtkModule_OnUpdate
     [FieldOffset(0x87F8)] internal fixed byte AddonAllocators[0x28 * 873];
-    [FieldOffset(0x11050)] public StdVector<Utf8String> AddonNames;
-    [FieldOffset(0x11068)] public AddonConfig* AddonConfigPtr;
+    // [FieldOffset(0x11060)] public StdVector<Utf8String> AddonNames;
+    // [FieldOffset(0x11078)] public AddonConfig* AddonConfigPtr;
 
     [FieldOffset(0x11120)] public UIModule* UIModulePtr;
     [FieldOffset(0x11128)] public RaptureLogModule* RaptureLogModulePtr;
